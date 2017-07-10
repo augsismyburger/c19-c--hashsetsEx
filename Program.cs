e2604c2043bd94593cbeccc88def3ccf9e894060
Matthew Augsburger <matthewaugsburger@gmail.com>
2017-07-10 09:17:08 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hashsets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> showRoom = new HashSet<string>();
+
+            showRoom.Add("Wrangler");
+            showRoom.Add("Cherokee");
+            showRoom.Add("Commanche");
+            showRoom.Add("Jeepster");
+            showRoom.Add("Wrangler");
+
+            Console.WriteLine(showRoom.Count);
+
+            HashSet<string> usedLot = new HashSet<string>();
+
+            usedLot.Add("Explorer");
+            usedLot.Add("Bronco");
+
+            showRoom.UnionWith(usedLot);
+
+            showRoom.Remove("Explorer");
+
+            HashSet<string> junkYard = new HashSet<string>();
+
+            // junkYard.Add("Wrangler");
+            junkYard.Add("F-150");
+            junkYard.Add("s-10");
+            junkYard.Add("Tundra");
+            junkYard.Add("LandRover");
+
+
+            // junkYard.IntersectWith(showRoom);
+
+            showRoom.UnionWith(junkYard);
+
+            showRoom.Remove("Tundra");
+
+            Console.WriteLine(showRoom.Count);
+
+            foreach (var item in showRoom)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
